3bc0e37281c6b25efbadd32364efac64aa9f8460
lykmapipo <lallyelias87@gmail.com>
2015-05-16 20:06:09 +0300

    fix seed generate for number, boolean, date and string

--- model/templates/_seed.js
+++ model/templates/_seed.js
@@ -1,13 +1,13 @@
 {<%_(fields).forEach(function (definition, field){
 	if(_.keys(fields).indexOf(field) < _.size(fields) - 1){%>
-	<% if(definition.type === 'String'){%>		<%= field %>: faker.internet.email(),<%}%>
-	<% if(definition.type === 'Date'){%>		<%= field %>: new Date(),<%}%>
-	<% if(definition.type === 'Number'){%>		<%= field %>: faker.random.number({ min:0}),<%}%>
-	<% if(definition.type === 'Boolean'){%>		<%= field %>: true,<%}%>
-	<%}else{%>
-	<% if(definition.type === 'String'){%>		<%= field %>: faker.internet.email()<%}%>
-	<% if(definition.type === 'Date'){%>		<%= field %>: new Date()<%}%>
-	<% if(definition.type === 'Number'){%>		<%= field %>: faker.random.number({ min:0})<%}%>
-	<% if(definition.type === 'Boolean'){%>		<%= field %>: true<%}%>
-	<%}%>
-<%});%>		}
+	<%if(definition.type === 'String'){%>		<%= field %>: faker.lorem.words(1)[0],<%}
+	if(definition.type === 'Date'){%>		<%= field %>: faker.date.past(4),<%}
+	if(definition.type === 'Number'){%>		<%= field %>: faker.random.number({ min:0}),<%}
+	if(definition.type === 'Boolean'){%>		<%= field %>: true,<%}
+	}else{%>
+	<%if(definition.type === 'Date'){%>		<%= field %>: faker.date.past(4)<%}
+	if(definition.type === 'Number'){%>		<%= field %>: faker.random.number({ min:0})<%}
+	if(definition.type === 'Boolean'){%>		<%= field %>: true<%}
+	}
+});%>
+		}
