32a33a1f8c81999f330efc4df8770a705673f3ae
lykmapipo <lallyelias87@gmail.com>
2015-05-16 20:09:47 +0300

    adding array and mixed type into model seed generator

--- model/templates/_seed.js
+++ model/templates/_seed.js
@@ -4,10 +4,14 @@
 	if(definition.type === 'Date'){%>		<%= field %>: faker.date.past(4),<%}
 	if(definition.type === 'Number'){%>		<%= field %>: faker.random.number({ min:0}),<%}
 	if(definition.type === 'Boolean'){%>		<%= field %>: true,<%}
+	if(definition.type === 'Array'){%>		<%= field %>: [{}],<%}
+	if(definition.type === 'Mixed'){%>		<%= field %>: {},<%}
 	}else{%>
 	<%if(definition.type === 'Date'){%>		<%= field %>: faker.date.past(4)<%}
 	if(definition.type === 'Number'){%>		<%= field %>: faker.random.number({ min:0})<%}
 	if(definition.type === 'Boolean'){%>		<%= field %>: true<%}
+	if(definition.type === 'Array'){%>		<%= field %>: []<%}
+	if(definition.type === 'Mixed'){%>		<%= field %>: []<%}
 	}
 });%>
 		}
