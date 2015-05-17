d9266ab2d1a8b46cd56ce8bf1f077d2b0d7077b8
lykmapipo <lallyelias87@gmail.com>
2015-05-17 15:04:40 +0300

    fix seed files

--- api/templates/_seed.js
+++ api/templates/_seed.js
@@ -7,7 +7,8 @@
 	if(definition.type === 'Array'){%>		<%= field %>: [{}],<%}
 	if(definition.type === 'Mixed'){%>		<%= field %>: {},<%}
 	}else{%>
-	<%if(definition.type === 'Date'){%>		<%= field %>: faker.date.past(4)<%}
+	<%if(definition.type === 'String'){%>		<%= field %>: faker.lorem.words(1)[0],<%}
+	if(definition.type === 'Date'){%>		<%= field %>: faker.date.past(4)<%}
 	if(definition.type === 'Number'){%>		<%= field %>: faker.random.number({ min:0})<%}
 	if(definition.type === 'Boolean'){%>		<%= field %>: true<%}
 	if(definition.type === 'Array'){%>		<%= field %>: []<%}
