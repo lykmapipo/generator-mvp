4d455b9c3fb81fbff94ba7ed4a19600581b4cf30
lykmapipo <lallyelias87@gmail.com>
2015-08-30 11:32:13 +0300

    update router spec templates

--- scaffold/templates/_router_spec.js
+++ scaffold/templates/_router_spec.js
@@ -11,18 +11,7 @@ var path = require('path');
 var expect = require('chai').expect;
 var faker = require('faker');
 var request = require('supertest');
-
-var app = require('express')();
-//parsing body
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-//TODO additional middlewares that will be require for <%= plural %> to work
-var <%= className %>Router = require(path.join(__dirname, '..', '..', 'app', 'routers', '<%=singular%>_router'));
-//add <%= className %>Router to app middlewares
-app.use(<%= className %>Router);
+var app = require(path.join(__dirname, '..', '..', 'app', 'application'));
 var <%= modelName %>;
 
 describe('<%= className %> Router', function() {
@@ -33,27 +22,10 @@ describe('<%= className %> Router', function() {
             .post('/<%=plural%>')
             .send(_<%= modelName %>_)
             .set('Accept', 'application/json')
-            .expect(200)
+            .expect(201)
             .expect('Content-Type', /json/)
             .end(function(error, response) {
                 <%= modelName %> = JSON.parse(response.text);
-                done(error, response);
-            });
-    });
-
-    
-    it('should be able to find <%= plural %> when http get on /<%=plural%>', function(done) {
-        request(app)
-            .get('/<%=plural%>')
-            .set('Accept', 'application/json')
-            .expect(200)
-            .expect('Content-Type', /json/)
-            .end(function(error, response) {
-
-                expect(error).to.be.null;
-                expect(response).to.not.be.null;
-                //TODO more <%=plural%> response assertions
-
                 done(error, response);
             });
     });
@@ -66,12 +38,13 @@ describe('<%= className %> Router', function() {
             .post('/<%=plural%>')
             .send(_<%= modelName %>_)
             .set('Accept', 'application/json')
-            .expect(200)
+            .expect(201)
             .expect('Content-Type', /json/)
             .end(function(error, response) {
 
-                expect(error).to.be.null;
-                expect(response).to.not.be.null;
+                expect(error).to.not.exist;
+                expect(response).to.exist;
+                
                 //TODO more <%=singular%> response assertions
 
                 done(error, response);
@@ -87,8 +60,9 @@ describe('<%= className %> Router', function() {
             .expect('Content-Type', /json/)
             .end(function(error, response) {
 
-                expect(error).to.be.null;
-                expect(response).to.not.be.null;
+                expect(error).to.not.exist;
+                expect(response).to.exist;
+
                 //TODO more <%=singular%> response assertions
 
                 done(error, response);
@@ -107,8 +81,9 @@ describe('<%= className %> Router', function() {
             .expect('Content-Type', /json/)
             .end(function(error, response) {
 
-                expect(error).to.be.null;
-                expect(response).to.not.be.null;
+                expect(error).to.not.exist;
+                expect(response).to.exist;
+
                 //TODO more <%=singular%> response assertions
 
                 done(error, response);
@@ -127,8 +102,9 @@ describe('<%= className %> Router', function() {
             .expect('Content-Type', /json/)
             .end(function(error, response) {
 
-                expect(error).to.be.null;
-                expect(response).to.not.be.null;
+                expect(error).to.not.exist;
+                expect(response).to.exist;
+
                 //TODO more <%=singular%> response assertions
 
                 done(error, response);
@@ -144,9 +120,28 @@ describe('<%= className %> Router', function() {
             .expect('Content-Type', /json/)
             .end(function(error, response) {
 
-                expect(error).to.be.null;
-                expect(response).to.not.be.null;
+                expect(error).to.not.exist;
+                expect(response).to.exist;
+
                 //TODO more <%=singular%> response assertions
+
+                done(error, response);
+            });
+    });
+
+
+    it('should be able to list <%= plural %> when http get on /<%=plural%>', function(done) {
+        request(app)
+            .get('/<%=plural%>')
+            .set('Accept', 'application/json')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end(function(error, response) {
+
+                expect(error).to.not.exist;
+                expect(response).to.exist;
+                
+                //TODO more <%=plural%> response assertions
 
                 done(error, response);
             });
