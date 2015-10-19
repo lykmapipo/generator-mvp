67ead26da42639f0ad3a0cafd3e5947cf46efd63
Benson Maruchu <bengenius12@gmail.com>
2015-10-19 13:40:32 +0300

    add pre_start_nodejs action hook

--- openshift/index.js
+++ openshift/index.js
@@ -7,13 +7,35 @@ var chalk = require('chalk');
 
 module.exports = yeoman.generators.base.extend({
 
-    prompting: function() {
-        var done = this.async();
+    prepare: function() {
+        //deduce database name from application name
+        var packageJson = {};
 
-        this.log(yosay(
-            'Welcome to Openshift ' + chalk.red('mvp') + 'generator'
-        ));
+        try {
+            packageJson = require(path.join(process.cwd(), 'package.json'));
+        } catch (e) {}
 
-        var prompts = []; // questions to interact with use
-    };
+        if (packageJson.name) {
+            this.applicationName = packageJson.name;
+        } else {
+            this.applicationName = path.basename(process.cwd());
+        }
+
+        this.databaseName = this.applicationName;
+
+    },
+
+    writing: {
+        openshift: function() {
+            //add openshift nodejs pre start hook
+            this.fs.copy(
+                this.templatePath('_pre_start_nodejs'),
+                this.destinationPath('.openshift/action_hooks/pre_start_nodejs')
+            );
+
+            //override production configurations
+            this.template('_production.js', 'app/config/production.js');
+        }
+    }
+
 });
