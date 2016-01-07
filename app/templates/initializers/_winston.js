dac675690e43a9011850ff60904567d220346e53
lykmapipo <lallyelias87@gmail.com>
2016-01-07 17:03:57 +0300

    update logger

--- app/templates/initializers/_winston.js
+++ app/templates/initializers/_winston.js
@@ -8,24 +8,34 @@
 var config = require('config');
 var path = require('path');
 var winston = require('winston');
+var isLocal = process.env.NODE_ENV && ((process.env.NODE_ENV === 'test') || (process.env.NODE_ENV === 'development'));
+var transports = [];
+
+//add development and test logger transports
+if (isLocal) {
+    transports = transports.push(new(winston.transports.Console)({
+        timestamp: true,
+        level: config.get('logger.level'),
+        color: true
+    }));
+}
+
+//add production logger transports
+if (!isLocal) {
+    transports = transports.push(new(winston.transports.File)({
+        timestamp: true,
+        level: config.get('logger.level'),
+        filename: path.join(
+            __dirname, '..', '..',
+            config.get('logger.dir'),
+            config.get('logger.file')
+        )
+    }));
+}
 
 var logger = new(winston.Logger)({
-    transports: [
-        new(winston.transports.Console)({
-            timestamp: true,
-            level: config.get('logger.level'),
-            color: true
-        }),
-        new(winston.transports.File)({
-            timestamp: true,
-            level: config.get('logger.level'),
-            filename: path.join(
-                __dirname, '..', '..',
-                config.get('logger.dir'),
-                config.get('logger.file')
-            )
-        })
-    ]
+    transports: transports
 });
 
+//export application logger
 module.exports = logger;
